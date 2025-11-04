0ec81830b2a5b1b03860de5dd668efa65a08a1d3
slim <slimabida21@gmail.com>
2025-11-04 13:44:56 +0100

    création du model de la collection ThreadSchema pour la récupération des discussions dans la base de donnée Forum-express

---
+++ models/Thread.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+// création du schéma de la discussion //
+const threadSchema = new mongoose.Schema(
+  {
+    // titre de la discussion //
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Le titre doit contenir au moins 3 caractères"],
+      maxlength: [100, "Le titre doit contenir au plus 100 caractères"],
+    },
+    // slug est un champ calculé automatiquementil sert a générer une url unique pour la discussion//
+    slug: {
+      type: String,
+      unique: true,
+    },
+    // compteur de post //
+    postCount: {
+      type: Number,
+      default: 1,
+    },
+    // date de la dernière publication //
+    lastPostAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  // timestamps: true pour ajouter automatiquement les champs createdAt et updatedAt //
+  { timestamps: true }
+);
+
+// création du modèle de la discussion //
+const Thread = mongoose.model("Thread", threadSchema);
+
+export default Thread;
